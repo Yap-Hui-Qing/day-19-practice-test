f3b145efeae874ad1d85fdf34830683ffb17e771
Yap-Hui-Qing <qing19772001@gmail.com>
2024-12-03 13:49:32 +0800

    initial commit

---
+++ src/main/java/vttp/batchb/ssf/day19_practice_test/Day19PracticeTestApplication.java
@@ -0,0 +1,13 @@
+package vttp.batchb.ssf.day19_practice_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day19PracticeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day19PracticeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/batchb/ssf/day19_practice_test/Day19PracticeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.batchb.ssf.day19_practice_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day19PracticeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
